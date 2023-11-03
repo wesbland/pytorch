c912f566868a78c63a5a1f988d4df9f3c124e23a
Richard Zou <rzou@fb.com>
2023-11-03 06:46:29 -0700

    Add generated opcheck test for if the pt2_compliant_tag is incorrectly applied (#112759)
    Summary:
    
    If there are xfails in the failures_dict and the operator has the
    pt2_compliant_tag, then we raise an error. These generated tests are separate
    from those in the failures dict because we don't actually need any sample
    inputs to check this.
    
    Test Plan: - New tests
    
    Differential Revision: D50936201

--- torch/testing/_internal/optests/generate_tests.py
+++ torch/testing/_internal/optests/generate_tests.py
@@ -7,6 +7,7 @@ import os
 import re
 import tempfile
 import threading
+import unittest
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import torch
@@ -252,6 +253,54 @@ def generate_opcheck_tests(
     for attr in test_methods:
         for prefix, tester in test_utils.items():
             construct_method(attr, prefix, tester)
+
+    generate_tag_tests(testcase, failures_dict, additional_decorators)
+
+
+def generate_tag_tests(testcase, failures_dict, additional_decorators):
+    def generate_test(qualname, definitely_not_pt2_compliant, xfailed_tests):
+        def inner(self):
+            try:
+                op = torch._library.utils.lookup_op(qualname)
+            except AttributeError as e:
+                # Operator not importable in this test file
+                raise unittest.SkipTest(f"Can't import operator {qualname}") from e
+            op_marked_as_compliant = torch.Tag.pt2_compliant_tag in op.tags
+            if not op_marked_as_compliant:
+                return
+            if not definitely_not_pt2_compliant:
+                return
+            raise AssertionError(
+                f"op '{qualname}' was tagged with torch.Tag.pt2_compliant_tag "
+                f"but it failed some of the generated opcheck tests "
+                f"({xfailed_tests}). This may lead to silent correctness issues, "
+                f"please fix this."
+            )
+
+        return inner
+
+    for qualname, test_dict in failures_dict.data.items():
+        xfailed_tests = [
+            test
+            for test, status_dict in test_dict.items()
+            # We're about to delete the following test after Ed's PR
+            # to specialize on C++ .size() calls
+            if "test_aot_dispatch_static" not in test
+            and status_dict["status"] == "xfail"
+        ]
+        definitely_not_pt2_compliant = len(xfailed_tests) > 0
+        generated = generate_test(qualname, definitely_not_pt2_compliant, xfailed_tests)
+
+        # Could result in collisions, but unlikely
+        mangled_qualname = qualname.replace("::", "_").replace(".", "_")
+        test_name = "test_pt2_compliant_tag_" + mangled_qualname
+
+        # You can skip this test via the additional_decorators argument
+        # in generate_opcheck_tests
+        if test_name in additional_decorators:
+            for decorator in additional_decorators[test_name]:
+                generated = decorator(generated)
+        setattr(testcase, test_name, generated)
 
 
 TEST_OPTIONS = ("xfail", "skip", "xsuccess")
